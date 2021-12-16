6770bca2837cc7d49ed7ec04fa924da12fa7ad20
Monique Neto <58047194+Monique1701@users.noreply.github.com>
2021-12-16 15:37:17 -0300

    Add files via upload

---
+++ SelecionadorDeData/app/src/main/java/com/example/selecionadordedata/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.selecionadordedata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initDatePicker();
+        dateButton = findViewById(R.id.datePickerButton);
+        dateButton.setText(getTodayDate());
+
+    }
+
+    private String getTodayDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month +1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+
+    }
+
+    private void initDatePicker() {
+
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this,style,dateSetListener,year,month,day);
+        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+    }
+
+    private String makeDateString(int day, int month, int year)
+    {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month)
+    {
+        if (month == 1)
+            return "JAN";
+        if (month == 2)
+            return "FEV";
+        if (month == 3)
+            return "MAR";
+        if (month == 4)
+            return "ABR";
+        if (month == 5)
+            return "MAI";
+        if (month == 6)
+            return "JUN";
+        if (month == 7)
+            return "JUL";
+        if (month == 8)
+            return "AGO";
+        if (month == 9)
+            return "SET";
+        if (month == 10)
+            return "OUT";
+        if (month == 11)
+            return "NOV";
+        if (month == 12)
+            return "DEZ";
+        return "DEZ";
+    }
+
+    public void openDatePicker(View v)
+    {
+        datePickerDialog.show();
+    }
+
+}
